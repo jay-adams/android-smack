312a87ec91fb649d7072363e383546f7f9563d13
Joseph Adams <jadams41@nyc.rr.com>
2019-11-09 10:25:46 -0500

    Add user login web request

--- app/src/main/java/com/tspcoders/smack/Utilities/Constants.kt
+++ app/src/main/java/com/tspcoders/smack/Utilities/Constants.kt
@@ -3,3 +3,4 @@ package com.tspcoders.smack.Utilities
 const val BASE_URL = "https://ptsmackchat.herokuapp.com/v1/"
 //const val BASE_URL = "http://10.0.2.2:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
