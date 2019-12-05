d9d5c4c2568d08a239c1a7c34137d2c79df4d535
Joseph Adams <jadams41@nyc.rr.com>
2019-12-05 05:01:10 -0500

    Add shared preferences

--- app/src/main/java/com/tspcoders/smack/Services/AuthService.kt
+++ app/src/main/java/com/tspcoders/smack/Services/AuthService.kt
@@ -10,14 +10,15 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.tspcoders.smack.Controller.App
 import com.tspcoders.smack.Utilities.*
 import org.json.JSONException
 
 object AuthService {
 
-    var isLoggedIn = false
+/*    var isLoggedIn = false
     var userEmail = ""
-    var authToken = ""
+    var authToken = ""*/
 
     fun registerUser(
         context: Context,
@@ -47,7 +48,7 @@ object AuthService {
                 }
             }
 
-        Volley.newRequestQueue(context).add(registerRequest)
+        App.prefs.requestQueue.add(registerRequest)
     }
 
     fun loginUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
@@ -60,9 +61,9 @@ object AuthService {
         val loginRequest =
             object : JsonObjectRequest(Method.POST, URL_LOGIN, null, Response.Listener { response ->
                 try {
-                    userEmail = response.getString("user")
-                    authToken = response.getString("token")
-                    isLoggedIn = true
+                    App.prefs.userEmail = response.getString("user")
+                    App.prefs.authToken = response.getString("token")
+                    App.prefs.isLoggedIn = true
                     complete(true)
                 } catch (e: JSONException) {
                     Log.d("JSON", "EXC:" + e.localizedMessage)
@@ -81,7 +82,7 @@ object AuthService {
                     return requestBody.toByteArray()
                 }
             }
-        Volley.newRequestQueue(context).add(loginRequest)
+        App.prefs.requestQueue.add(loginRequest)
     }
 
     fun createUser(
@@ -129,16 +130,16 @@ object AuthService {
 
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
-                headers.put("Authorization", "Bearer $authToken")
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
                 return headers
             }
         }
 
-        Volley.newRequestQueue(context).add(createRequest)
+        App.prefs.requestQueue.add(createRequest)
     }
 
     fun findUserByEmail(context: Context, complete: (Boolean) -> Unit) {
-        val findUserRequest = object: JsonObjectRequest(Method.GET, "$URL_GET_USER$userEmail", null, Response.Listener { response ->
+        val findUserRequest = object: JsonObjectRequest(Method.GET, "$URL_GET_USER${App.prefs.userEmail}", null, Response.Listener { response ->
 
             try {
                 UserDataService.name = response.getString("name")
@@ -165,11 +166,11 @@ object AuthService {
 
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
-                headers.put("Authorization", "Bearer $authToken")
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
                 return headers
             }
         }
 
-        Volley.newRequestQueue(context).add(findUserRequest)
+        App.prefs.requestQueue.add(findUserRequest)
     }
 }
--- app/src/main/java/com/tspcoders/smack/Services/UserDataService.kt
+++ app/src/main/java/com/tspcoders/smack/Services/UserDataService.kt
@@ -1,6 +1,7 @@
 package com.tspcoders.smack.Services
 
 import android.graphics.Color
+import com.tspcoders.smack.Controller.App
 import java.util.*
 
 object UserDataService {
@@ -39,9 +40,9 @@ object UserDataService {
         avatarName = ""
         email = ""
         name = ""
-        AuthService.authToken = ""
-        AuthService.userEmail = ""
-        AuthService.isLoggedIn = false
+        App.prefs.authToken = ""
+        App.prefs.userEmail = ""
+        App.prefs.isLoggedIn = false
     }
 }
 
