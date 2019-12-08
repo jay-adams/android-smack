c89a0345b6435b30da80cc8827168665e99400c1
Joseph Adams <jadams41@nyc.rr.com>
2019-12-08 17:04:26 -0500

    Add functions to handle new messages

--- app/src/main/java/com/tspcoders/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/tspcoders/smack/Controller/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.tspcoders.smack.Model.Channel
+import com.tspcoders.smack.Model.Message
 import com.tspcoders.smack.R
 import com.tspcoders.smack.Services.AuthService
 import com.tspcoders.smack.Services.MessageService
@@ -50,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         socket.connect()
         socket.on("channelCreated", onNewChannel)
+        socket.on("messageCreated", onNewMessage)
 
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -166,7 +168,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val onNewMessage = Emitter.Listener { args ->
+        runOnUiThread {
+            val msgBody = args[0] as String
+            val channelId = args[2] as String
+            val userName = args[3] as String
+            val userAvatar = args[4] as String
+            val userAvatarColor = args[5] as String
+            val id = args[6] as String
+            val timeStamp = args[7] as String
+
+            val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+            MessageService.messages.add(newMessage)
+            println(newMessage.message)
+        }
+    }
+
     fun sendMsgBtnClicked(view: View) {
+        if(App.prefs.isLoggedIn && messageTextField.text.isNotEmpty() && selectedChannel != null) {
+            val userId = UserDataService.id
+            val channelId = selectedChannel!!.id
+            socket.emit("newMessage", messageTextField.text.toString(), userId, channelId,
+                UserDataService.name, UserDataService.avatarName, UserDataService.avatarColor)
+            messageTextField.text.clear()
+            hideKeyboard()
+        }
         hideKeyboard()
     }
 
